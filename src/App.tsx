1050767b2048aa830c96bfdb7bed9408d102d21f
Shouqiang Li <shouqiangli@Shouqiangs-MacBook-Pro.local>
2025-04-15 23:33:24 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Home page</h1>
+
+        </div>
+
+        <Button>Hello world</Button>
+
+      </main>
+    </>
+  )
+}
+
+export default App
