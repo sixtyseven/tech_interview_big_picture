30bfcb76ed1d8fede286d33cd04d39105dea5ca7
Shouqiang Li <shouqiangli@Shouqiangs-MacBook-Pro.local>
2025-04-16 14:15:20 +1000

    Tech interview: added a simple form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import { Button } from 'react-bootstrap';
+// import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function App() {
-
   return (
     <>
       <main>
-        <div>
-          <h1>Home page</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="formFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="First Name"
+              required
+              minLength={3}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formMiddleName">
+            <Form.Label>Middle Name</Form.Label>
+            <Form.Control type="text" placeholder="Middle Name" minLength={3} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formSirName">
+            <Form.Label>Sir Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Sir Name"
+              required
+              minLength={3}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formMobileNumber">
+            <Form.Label>Mobile Number</Form.Label>
+            <Form.Control
+              type="tel"
+              placeholder="Mobile Number"
+              required
+              pattern="\+614[0-9]{8}"
+            />
+          </Form.Group>
 
-        </div>
-
-        <Button>Hello world</Button>
-
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
